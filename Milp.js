7d5942046214a80e4ee7768c3aa221c5c37617e6
Shaun <sman1@users.noreply.github.com>
2021-02-20 22:30:26 -0500

    Update Milp.js

--- Milp.js
+++ Milp.js
@@ -1,52 +1,89 @@
-import solver from "javascript-lp-solver";
-import data from "./foods.json";
+let solver = require("javascript-lp-solver");
+let data = require("./foods.json");
 
-let foods = {};
-let clonedFoods;
-
-class LP {
-
-    constructor() {
+class DietOptimizer {
+    goals;
+    solutions = [];
+    days;
+    solved = false;
+    constructor(options, days) {
+        this.goals = options;
+        this.days = days;
     }
+    solveDay = () => {
 
-    process() {
-
-        this.result();
-    }
-
-    result() {
-
+        let foods = {};
+        let intVars = {};
+        let servingConstr = {}
         const shuffled = Object.keys(data).sort(() => 0.5 - Math.random());
+        // Get sub-array of first n elements after shuffled then propagate empty objects
+        shuffled.slice(0, 100).forEach((item) => {
+            foods[item] = data[item];
+            intVars[item] = 1;
+            servingConstr[item] = { "max": 1.1 }; //no "priority"
+        });
 
-        // Get sub-array of first n elements after shuffled
-        let selected = shuffled.slice(0, 100).forEach(item => foods[item] = data[item]);
-
-
-        clonedFoods = JSON.parse(JSON.stringify(foods));
-
-        console.log(foods)
-        
-        let intVars = Object.keys(clonedFoods).forEach(v => clonedFoods[v] = 1);
         let model = {
+
             "opType": "min",
             "optimize": "quantity",
             "constraints": {
-                "calories": { "max": 2200, "min": 1800 },
-                "quantity": { "max": 5, "min": 3 }
+                ...this.goals,
+                //"quantity": { "max": 5, "min": 3 }
+                ...servingConstr
             },
             "variables": foods,
-            "ints": clonedFoods,
+            "ints": intVars,
             "options": {
-                "tolerance": 0.05
+                "tolerance": 0.05,
+                "keep_solutions": true
+            }
+
+        };
+        let ans = solver.Solve(model, 1e-8, true);
+        //console.log(model)
+        let result = ans.solutionSet;
+        //console.log(ans._tableau.model.solutions);
+
+        let satisfied = true;
+
+        for (const key in result) {
+            if (result[key] > 1) {
+                console.log("The solution is not binary, let's try again")
+                satisfied = false;
+                break;
             }
         }
-        console.log(model)
-        let ans = solver.Solve(model)
 
-        console.log(ans);
+        if (!satisfied) {
+            this.solveDiet() //let's try again
+        }
+        else {
+            this.solutions.push(result);
+            if (this.solutions.length >= this.days){
+                this.solved = true;
+            }
+            console.log(result);
+        }
+
     }
 
+    gatherDays = () => {
+        let n = this.days;
+        while (n > 0){
+            this.solveDay();
+            n--;
+        }
+    }
 
 }
 
-export default LP;
+opts = {
+    "calories": { "max": 2000, "min": 1500 },
+    "carbohydrates": { "min": 270, "max": 370 },
+    "protein": { "min": 70, "max": 90 },
+    "fat": { "min": 50, "max": 70 },
+}
+plan = new DietOptimizer(opts)
+plan.gatherDays();
+console.log(plan.solutions)
